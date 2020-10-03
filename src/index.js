13d85297da9677ec9e5dcb214f3b42dd3de6ce21
Adam Meadows <adammeadows@Adams-MacBook-Air.local>
2020-10-03 00:18:57 -0700

    api started

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import dotenv from 'dotenv';
+import path from 'path';
 
 import typeDefs from './schemas/schema';
 import resolvers from './resolvers/resolvers';
@@ -15,7 +16,12 @@ const server = new ApolloServer({
 });
 
 server.applyMiddleware({ app, path: '/api' });
+app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, '../build/rkt')));
 
-app.get('/', (req, res) => { res.send('Hello world'); });
+const rktPath = path.join(__dirname, '../build/rkt');
+app.get('/rkt', (req, res) => {
+  res.sendFile(path.join(rktPath, 'rkt.html'));
+});
 
 app.listen({ port: 4000 }, () => { console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`); });
